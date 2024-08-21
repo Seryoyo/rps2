d63be66fef31a129acc4d2e6a23c1d25d22956de
Seryoyo <harogio101@gmail.com>
2024-08-20 20:21:04 -0700

    Create basic RPS game on console

---
+++ script.js
@@ -0,0 +1,90 @@
+
+
+function getComputerChoice(){
+
+    let com = Math.floor(Math.random() * 3) + 1;
+
+    if(com == 1){
+        return "Rock";
+    }
+    else if(com == 2){
+        return "Paper";
+    }
+    else if(com == 3){
+        return "Scissors";
+    }
+
+}
+
+function getHumanChoice(){
+
+    let hum = prompt("Rock, Paper, Or Scissors?");
+
+    return hum.slice(0,1).toUpperCase() + hum.slice(1).toLowerCase();
+    
+}
+
+
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice == computerChoice){
+        console.log("It's a Tie");
+    }
+    else if(humanChoice === "Rock" && computerChoice === "Paper"){
+        computerScore++;
+        console.log("You lose, Rock loses to Paper");
+    }
+    else if(humanChoice === "Rock" && computerChoice === "Scissors"){
+        humanScore++;
+        console.log("You win, Rock beats Scissors");
+    }
+    else if(humanChoice === "Paper" && computerChoice === "Rock"){
+        humanScore++;
+        console.log("You win, Paper beats Rock");
+    }
+    else if(humanChoice === "Paper" && computerChoice === "Scissors"){
+        computerScore++;
+        console.log("You lose, Paper loses to Scissors");
+    }
+    else if(humanChoice === "Scissors" && computerChoice === "Paper"){
+        humanScore++;
+        console.log("You win, Scissors beats Paper");
+    }
+    else if(humanChoice ==="Scissors" && computerChoice === "Rock"){
+        computerScore++;
+        console.log("You lose, Scissors loses to Rock");
+    }
+
+    console.log("Human: " + humanScore +"   Computer: " + computerScore);
+    console.log("");
+
+}
+
+
+function playGame(){
+
+    for(let i = 0; i < 5; i++){
+
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
+
+    }
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+
+
+playGame();
+
+
+
+
+
+
+
+
+
